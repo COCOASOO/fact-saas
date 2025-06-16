810b6d8cf6ac5bbdde39ffc2f93b3cca6d74b985
Adrian <adrianalarcon720@gmail.com>
2025-06-16 11:05:28 +0200

    testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/fact-saas',
   // Eliminamos swcMinify ya que no es necesario en las versiones recientes
   // Eliminamos appDir ya que no es una opción válida
 }
